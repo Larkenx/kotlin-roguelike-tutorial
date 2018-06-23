c67cdf6cd5e4ca9ac32e39d143e77491304f734a
larkenx <larkenx@gmail.com>
2018-06-22 21:47:04 -0400

    drawing a cyan @ to the screen and drawing a border around the game is now done!

--- core/src/com/mygdx/game/MyGdxGame.kt
+++ core/src/com/mygdx/game/MyGdxGame.kt
@@ -1,30 +1,52 @@
 package com.mygdx.game
 
 import com.badlogic.gdx.ApplicationAdapter
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.halfdeadgames.kterminal.KTerminalData
+import com.halfdeadgames.kterminal.KTerminalGlyph
+import com.halfdeadgames.kterminal.KTerminalRenderer
+import ktx.app.clearScreen
+import ktx.app.use
 
 class MyGdxGame : ApplicationAdapter() {
     lateinit var batch: SpriteBatch
-    lateinit var img: Texture
+    lateinit var terminalData: KTerminalData
+    lateinit var terminalRenderer: KTerminalRenderer
+    var width = 50
+    var height = 26
 
     override fun create() {
         batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+
+        terminalData = KTerminalData(width, height, Color.WHITE, Color.BLACK)
+        terminalRenderer = KTerminalRenderer("fontSheet.png", 1f, batch)
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
+        clearScreen(0f, 0f, 0f, 1f)
+        terminalData.clearAll()
+        /*  Drawing Code goes here */
+        terminalData.resetCursor()
+        terminalData[0, 0].drawBox(
+                width = width,
+                height = height,
+                topRight = KTerminalData.BOX_DOUBLE_DOWN_LEFT,
+                bottomLeft = KTerminalData.BOX_DOUBLE_UP_RIGHT,
+                bottomRight = KTerminalData.BOX_DOUBLE_UP_LEFT,
+                topLeft = KTerminalData.BOX_DOUBLE_DOWN_RIGHT,
+                horizontal = KTerminalData.BOX_DOUBLE_HORIZONTAL,
+                vertical = KTerminalData.BOX_DOUBLE_VERTICAL
+        )
+        terminalData[width / 2, height / 2].write(KTerminalGlyph('@', Color.CYAN, Color.BLACK))
+        /* End drawing code */
+        batch.use {
+            terminalRenderer.render(0f, 0f, terminalData)
+        }
     }
 
     override fun dispose() {
         batch.dispose()
-        img.dispose()
+        terminalRenderer.dispose()
     }
 }
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.kt
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.kt
@@ -8,6 +8,12 @@ object DesktopLauncher {
     @JvmStatic
     fun main(arg: Array<String>) {
         val config = LwjglApplicationConfiguration()
+        config.title = "Kotlin Roguelike Tutorial"
+        config.width = 800
+        config.height = 416
+        config.backgroundFPS = 60
+        config.foregroundFPS = 60
+        config.resizable = false
         LwjglApplication(MyGdxGame(), config)
     }
 }
